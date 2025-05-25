7c7e70b175be1dbf234b644545d4e4087167b2fc
mohammad <mfelfelani72@gmail.com>
2025-05-25 07:12:42 +0000

    update

--- app/Utils/services/api/api.js
+++ app/Utils/services/api/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8002/api",
+  baseURL: "http://localhost:8000/api",
 
-  // headers: {
-  //   "Accept-Version": 1,
-  //   Accept: "application/json",
-  //   "Access-Control-Allow-Origin": "*",
-  //   "Content-Type": "application/json; charset=utf-8",
-  // },
+  headers: {
+    "Accept-Version": 1,
+    Accept: "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json; charset=utf-8",
+  },
 
-  withCredentials: false,
-  withXSRFToken: false,
+  withCredentials: true,
+  withXSRFToken: true,
 });
