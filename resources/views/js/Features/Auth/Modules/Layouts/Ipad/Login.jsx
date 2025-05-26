e9be4f10d4a09da3974ccea505a15059b0b1d43e
mohammad <mfelfelani72@gmail.com>
2025-05-26 16:33:17 +0000

    responsive auth

--- resources/views/js/Features/Auth/Modules/Layouts/Ipad/Login.jsx
+++ resources/views/js/Features/Auth/Modules/Layouts/Ipad/Login.jsx
@@ -40,74 +40,77 @@ const Login = () => {
 
                 {/* login form */}
 
-                <div className='w-[32rem] max-h-[42rem] h-full rounded-4xl bg-gray-50 inline-flex flex-col justify-center items-center px-14 py-8 shadow-xl z-30'>
+                <div className='w-[32rem] max-h-[42rem] h-full rounded-4xl bg-gray-50 inline-flex flex-col justify-between items-center px-14 py-8 shadow-xl z-30'>
 
-                    <Logo width="36" height="48" />
+                    <div className='flex flex-col w-full items-center'>
+                        <Logo width="36" height="48" />
 
-                    <h1 className='font-bold text-2xl'>{t("login")}</h1>
+                        <h1 className='font-bold text-2xl'>{t("login")}</h1>
 
-                    <GoogleButton type={"login"} classname={"py-2 mt-4"} />
+                        <GoogleButton type={"login"} classname={"py-2 mt-4"} />
 
-                    {/* line    Or    line */}
+                        {/* line    Or    line */}
 
-                    <div className='flex flex-row mt-3 gap-6 items-center'>
-                        <div className='w-28 h-0 outline outline-gray-200'></div>
-                        <div className='font-medium text-sm text-gray-500'>{t("or")}</div>
-                        <div className='w-28 h-0 outline outline-gray-200'></div>
-                    </div>
-
-                    {/* form */}
-                    <div className='flex flex-col items-start w-full mt-1'>
-                        <div className='w-full'>
-                            <InputEmail id="email" label={t("email_address")} placeholder="mfelfelani72@gmail.com" theme="light" />
-                        </div>
-                        <div className='mt-4 w-full'>
-                            <InputPassword
-                                id="password"
-                                type="password"
-                                label={t("password")}
-                                theme={"light"}
-                                button_first_id="login"
-                                button_secound_id="login_disable"
-                                onChange={(event) => {
-                                    EnableButtonControl(event, "login", "login_disable");
-                                }}
-                                error={errors["password"]}
-                            />
+                        <div className='flex flex-row mt-3 gap-6 items-center'>
+                            <div className='w-28 h-0 outline outline-gray-200'></div>
+                            <div className='font-medium text-sm text-gray-500'>{t("or")}</div>
+                            <div className='w-28 h-0 outline outline-gray-200'></div>
                         </div>
 
-                        <div className='w-full mt-10'>
-                            <SubmitButton
-                                id="login"
-                                onClick={(e) => {
-                                    handleClick();
-                                }}
-                                className="hidden relative"
-                            >
-                                <div className=''>
-                                    {t("login")}
+                        {/* form */}
+                        <div className='flex flex-col items-start w-full mt-1'>
+                            <div className='w-full'>
+                                <InputEmail id="email" label={t("email_address")} placeholder="mfelfelani72@gmail.com" theme="light" />
+                            </div>
+                            <div className='mt-4 w-full'>
+                                <InputPassword
+                                    id="password"
+                                    type="password"
+                                    label={t("password")}
+                                    theme={"light"}
+                                    button_first_id="login"
+                                    button_secound_id="login_disable"
+                                    onChange={(event) => {
+                                        EnableButtonControl(event, "login", "login_disable");
+                                    }}
+                                    error={errors["password"]}
+                                />
+                            </div>
+
+                            <div className='w-full mt-10'>
+                                <SubmitButton
+                                    id="login"
+                                    onClick={(e) => {
+                                        handleClick();
+                                    }}
+                                    className="hidden relative"
+                                >
+                                    <div className=''>
+                                        {t("login")}
+                                    </div>
+                                    {sendRequest && <LoaderDotSpinner className={"rtl:left-0 ltr:right-0"} />}
+                                </SubmitButton>
+                                <SubmitButton
+                                    id="login_disable"
+                                    className="flex bg-gray-400 pointer-events-none cursor-default"
+                                >
+                                    <div className=''>
+                                        {t("login")}
+                                    </div>
+                                </SubmitButton>
+                            </div>
+
+                            <div className='flex flex-row w-full justify-center gap-1 mt-2'>
+                                <div className='text-xs text-gray-900'>
+                                    {t("dont_have_account")}
                                 </div>
-                                {sendRequest && <LoaderDotSpinner className={"rtl:left-0 ltr:right-0"} />}
-                            </SubmitButton>
-                            <SubmitButton
-                                id="login_disable"
-                                className="flex bg-gray-400 pointer-events-none cursor-default"
-                            >
-                                <div className=''>
-                                    {t("login")}
-                                </div>
-                            </SubmitButton>
+                                <TextButton onClick={() => navigate("register")} className="text-xs" text={t("register")} />
+                            </div>
                         </div>
+
                     </div>
 
                     {/*  */}
-
-                    <div className='flex flex-row gap-1 mt-2'>
-                        <div className='text-xs text-gray-900'>
-                            {t("dont_have_account")}
-                        </div>
-                        <TextButton onClick={() => navigate("register")} className="text-xs" text={t("register")} />
-                    </div>
 
                     <div className='flex flex-row gap-2 mt-2'>
                         <FaceBookIcon width="32" height="32" color={"#686677"} />
@@ -115,7 +118,6 @@ const Login = () => {
                         <InstagramIcon width="32" height="32" color={"#686677"} />
                         <LinkdinIcon width="32" height="32" color={"#686677"} />
                     </div>
-
                 </div>
             </div>
 
