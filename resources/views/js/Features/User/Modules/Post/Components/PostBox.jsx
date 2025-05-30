ec97f5710d2e5387f90c55a47e53f68b95dca678
mohammad <mfelfelani72@gmail.com>
2025-05-30 08:31:41 +0000

    update blog

--- resources/views/js/Features/User/Modules/Post/Components/PostBox.jsx
+++ resources/views/js/Features/User/Modules/Post/Components/PostBox.jsx
@@ -7,7 +7,7 @@ const PostBox = ({ className, ...props }) => {
         <div
             {...props}
             className={cn(
-                "flex flex-col bg-gray-50 shadow-sm rounded-lg max-h-[20rem] min-h-[23rem] transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl overflow-hidden cursor-pointer",
+                "group flex flex-col bg-gray-50 shadow-sm rounded-lg max-h-[20rem] min-h-[23rem] transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl overflow-hidden cursor-pointer",
                 className
             )}
         >
@@ -29,7 +29,7 @@ const PostBox = ({ className, ...props }) => {
                     <h3 className='font-medium hover:text-violet-600'>this is a title</h3>
                     <p className='text-gray-400 text-sm pt-1'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat quae nobis dolor possimus. Animi, saepe? Ipsam nam cum dignissimos labore ipsa delectus necessitatibus tempore asperiores eligendi deserunt a, amet ex.</p>
                 </div>
-                <div className='flex flex-row-reverse items-center gap-1'>
+                <div className='flex flex-row-reverse items-center gap-1 group-hover:animate-bounce'>
                     <div className='text-xs text-gray-500'><span className='font-bold'>15</span> min</div>
                     <ClockIcon className={"h-4 w-4 text-gray-400"} />
                 </div>
