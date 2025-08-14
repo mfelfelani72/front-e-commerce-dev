b0038f35c90c281000d849415b60beb3586dce16
mohammad <mfelfelani72@gmail.com>
2025-08-14 12:37:13 +0330

    update post page

--- resources/views/js/Features/User/Modules/Post/Containers/Post.jsx
+++ resources/views/js/Features/User/Modules/Post/Containers/Post.jsx
@@ -7,6 +7,10 @@ import { ClockIcon } from "../../../../../Core/Components/Icon";
 import ShortPostBox from "../Components/ShortPostBox";
 import CategoryList from "../Components/CategoryList";
 
+// png
+
+import thumbnail from "../../../../../../../../public/assets/image/blog-thumbnail.jpg";
+
 const Post = ({ className, ...props }) => {
     return (
         <>
@@ -39,11 +43,21 @@ const Post = ({ className, ...props }) => {
                             className=""
                         >
                             <div className="flex flex-col py-4 gap-2">
-                                <ShortPostBox className={"border-t border-gray-300 pt-2"} />
-                                <ShortPostBox className={"border-t border-gray-300 pt-2"} />
-                                <ShortPostBox className={"border-t border-gray-300 pt-2"} />
-                                <ShortPostBox className={"border-t border-gray-300 pt-2"} />
-                                <ShortPostBox className={"border-t border-gray-300 pt-2"} />
+                                <ShortPostBox
+                                    className={"border-t border-gray-300 pt-2"}
+                                />
+                                <ShortPostBox
+                                    className={"border-t border-gray-300 pt-2"}
+                                />
+                                <ShortPostBox
+                                    className={"border-t border-gray-300 pt-2"}
+                                />
+                                <ShortPostBox
+                                    className={"border-t border-gray-300 pt-2"}
+                                />
+                                <ShortPostBox
+                                    className={"border-t border-gray-300 pt-2"}
+                                />
                             </div>
                         </Accordion>
 
@@ -55,20 +69,127 @@ const Post = ({ className, ...props }) => {
                             className=""
                         >
                             <div className="flex flex-col py-4">
-                                <CategoryList className={"border-t border-gray-300 py-4"} title={"Dairy & Milk"} count="24" />
-                                <CategoryList className={"border-t border-gray-300 py-4"} title={"Dairy & Milk"} count="24" />
-                                <CategoryList className={"border-t border-gray-300 py-4"} title={"Dairy & Milk"} count="24" />
-                                <CategoryList className={"border-t border-gray-300 py-4"} title={"Dairy & Milk"} count="24" />
-                                <CategoryList className={"border-t border-gray-300 py-4"} title={"Dairy & Milk"} count="24" />
-                                <CategoryList className={"border-t border-gray-300 py-4"} title={"Dairy & Milk"} count="24" />
-                                <CategoryList className={"border-t border-gray-300 py-4"} title={"Dairy & Milk"} count="24" />
-                                <CategoryList className={"border-t border-gray-300 py-4"} title={"Dairy & Milk"} count="24" />
-                                
+                                <CategoryList
+                                    className={"border-t border-gray-300 py-4"}
+                                    title={"Dairy & Milk"}
+                                    count="24"
+                                />
+                                <CategoryList
+                                    className={"border-t border-gray-300 py-4"}
+                                    title={"Dairy & Milk"}
+                                    count="24"
+                                />
+                                <CategoryList
+                                    className={"border-t border-gray-300 py-4"}
+                                    title={"Dairy & Milk"}
+                                    count="24"
+                                />
+                                <CategoryList
+                                    className={"border-t border-gray-300 py-4"}
+                                    title={"Dairy & Milk"}
+                                    count="24"
+                                />
+                                <CategoryList
+                                    className={"border-t border-gray-300 py-4"}
+                                    title={"Dairy & Milk"}
+                                    count="24"
+                                />
+                                <CategoryList
+                                    className={"border-t border-gray-300 py-4"}
+                                    title={"Dairy & Milk"}
+                                    count="24"
+                                />
+                                <CategoryList
+                                    className={"border-t border-gray-300 py-4"}
+                                    title={"Dairy & Milk"}
+                                    count="24"
+                                />
+                                <CategoryList
+                                    className={"border-t border-gray-300 py-4"}
+                                    title={"Dairy & Milk"}
+                                    count="24"
+                                />
                             </div>
                         </Accordion>
                     </div>
                     {/* body */}
-                    <div className="flex flex-col w-full">body</div>
+                    <div className="flex flex-col gap-3 w-full">
+                        <img
+                            src={thumbnail}
+                            alt="thumbnail"
+                            className="rounded-sm"
+                            loading="lazy"
+                        />
+                        <label className="text-md text-gray-500">
+                            June 30,2022 - Category
+                        </label>
+                        <h3 className="text-2xl font-bold text-gray-700">
+                            Marketing Guide: 5 Steps to Success.
+                        </h3>
+                        <div className="flex flex-col gap-6">
+                            <p className="text-gray-600">
+                                "Lorem Ipsum is simply dummy text of the
+                                printing and typesetting industry. Lorem Ipsum
+                                has been the industry's standard dummy text ever
+                                since the 1500s, when an unknown printer took a
+                                galley of type and scrambled it to make a type
+                                specimen book. It has survived not only five
+                                centuries, but also the leap into electronic
+                                typesetting, remaining essentially unchanged. It
+                                was popularised in the 1960s with the release of
+                                Letraset sheets containing Lorem Ipsum passages,
+                                and more recently with desktop publishing
+                                software like Aldus PageMaker including versions
+                                of Lorem Ipsum.
+                            </p>
+                            <p className="text-gray-600">
+                                The standard chunk of Lorem Ipsum used since the
+                                1500s is reproduced below for those interested.
+                                Sections 1.10.32 and 1.10.33 from "de Finibus
+                                Bonorum et Malorum" by Cicero.
+                            </p>
+                            <p className="text-gray-600">
+                                Contrary to popular belief, Lorem Ipsum is not
+                                simply random text. It has roots in a piece of
+                                passages of Lorem Ipsum classical Latin
+                                literature from 45 BC, making it over 2000 years
+                                old. Richard McClintock, a Latin professor at
+                                Hampden-Sydney College in Virginia
+                            </p>
+                            <p className="text-gray-600">
+                                Contrary to popular belief, Lorem Ipsum is not
+                                simply random text. It has roots in a piece of
+                                passages of Lorem Ipsum classical Latin
+                                literature from 45 BC, making it over 2000 years
+                                old. Richard McClintock, a Latin professor at
+                                Hampden-Sydney College in Virginia
+                            </p>
+                            <p className="text-gray-600">
+                                Contrary to popular belief, Lorem Ipsum is not
+                                simply random text. It has roots in a piece of
+                                passages of Lorem Ipsum classical Latin
+                                literature from 45 BC, making it over 2000 years
+                                old. Richard McClintock, a Latin professor at
+                                Hampden-Sydney College in Virginia
+                            </p>
+                            <p className="text-gray-600">
+                                Contrary to popular belief, Lorem Ipsum is not
+                                simply random text. It has roots in a piece of
+                                passages of Lorem Ipsum classical Latin
+                                literature from 45 BC, making it over 2000 years
+                                old. Richard McClintock, a Latin professor at
+                                Hampden-Sydney College in Virginia
+                            </p>
+                            <p className="text-gray-600">
+                                Contrary to popular belief, Lorem Ipsum is not
+                                simply random text. It has roots in a piece of
+                                passages of Lorem Ipsum classical Latin
+                                literature from 45 BC, making it over 2000 years
+                                old. Richard McClintock, a Latin professor at
+                                Hampden-Sydney College in Virginia
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
